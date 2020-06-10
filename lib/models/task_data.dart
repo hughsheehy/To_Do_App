bf5649cd28ace0253d823a2531346f6dcf974812
hugh sheehy <hughsheehy@users.noreply.github.com>
2020-06-10 15:01:00 +0100

    readme
    start

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -4,9 +4,9 @@ import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
   List<Task> _tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
+    Task(name: 'Go kitesurfing'),
+    Task(name: 'Write another Medium article'),
+    Task(name: 'Lift more weight'),
   ];
 
   UnmodifiableListView<Task> get tasks {
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -47,7 +47,7 @@ class TasksScreen extends StatelessWidget {
                   height: 10.0,
                 ),
                 Text(
-                  'Todoey',
+                  'Hugh\'s Simple To-do App',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 50.0,
